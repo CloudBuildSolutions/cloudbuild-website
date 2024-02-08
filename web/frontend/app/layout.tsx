d64d07b04abfa75522f3ce4618fb731c7cad1e49
Mark Koberlein <mkoberlein@gmail.com>
2024-02-07 21:01:57 -0500

    finished home page content

--- web/frontend/app/layout.tsx
+++ web/frontend/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { interFont } from "../lib/fonts";
+import { cn } from "../lib/utils";
+
+import "../styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Cloud Build Solutions | Redefining Solution Architecture for the Agile Enterprise",
@@ -15,14 +16,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={cn(interFont.className, "bg-gradient-to-r from-red-700 via-red-500 to-red-700")}>{children}</body>
     </html>
   );
 }
